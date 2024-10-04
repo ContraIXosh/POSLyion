37cbe893ab146e39bcf8f8ebd905857e76500271
Emmau <villalbamauricio_5@hotmail.com>
2024-10-04 16:33:46 -0300

    CRUD Clientes

--- PDVLyion/formCustomers.cs
+++ PDVLyion/formCustomers.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using EntityLayer;
+using BusinessLayer;
 
 namespace PDCLyion
 {
@@ -89,6 +91,70 @@ namespace PDCLyion
         private void panel_footer_Paint(object sender, PaintEventArgs e)
         {
 
+        }
+
+        private void formCustomers_Load(object sender, EventArgs e)
+        {
+            List<Customers> customer_list = new BL_Customers().ListAll();
+            foreach(Customers customer in customer_list)
+            {
+                grid_proveedores.Rows.Add(new object[]
+                {
+                    "",
+                    customer.Customer_id,
+                    customer.Full_name,
+                    customer.Dni,
+                    customer.Phone,
+                    customer.Email,
+                    customer.State == true ? "Activo" : "Inactivo",
+                    customer.State == true ? 1 : 0
+                });
+            }
+        }
+
+        private void grid_proveedores_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int index = e.RowIndex;
+            if (grid_proveedores.Columns[e.ColumnIndex].Name == "btn_editar")
+            {
+                if (index >= 0)
+                {
+                    Customers customer = new Customers()
+                    {
+                        Customer_id = Convert.ToInt32(grid_proveedores.Rows[index].Cells["cliente_id"].Value),
+                        Dni = grid_proveedores.Rows[index].Cells["cliente_dni"].Value.ToString(),
+                        Full_name = grid_proveedores.Rows[index].Cells["cliente_fullname"].Value.ToString(),
+                        Email = grid_proveedores.Rows[index].Cells["cliente_email"].Value.ToString(),
+                        Phone = grid_proveedores.Rows[index].Cells["cliente_tel"].Value.ToString(),
+                        State = Convert.ToBoolean(grid_proveedores.Rows[index].Cells["cliente_estado_valor"].Value)
+                    };
+                    abrirHerencia(new formCustomers2(customer));
+                }
+            }
+            else if (grid_proveedores.Columns[e.ColumnIndex].Name == "btn_eliminar")
+            {
+                if (MessageBox.Show("¿Desea eliminar el cliente", "Mensaje", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    if (index >= 0)
+                    {
+                        string message = string.Empty;
+                        Customers customer = new Customers()
+                        {
+                            Customer_id = Convert.ToInt32(grid_proveedores.Rows[index].Cells["cliente_id"].Value)
+                        };
+                        bool result = new BL_Customers().Delete(customer, out message);
+                        if (!result)
+                        {
+                            MessageBox.Show(message, "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Cliente eliminado con exito", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                            abrirHerencia(new formCustomers2());
+                        }
+                    }
+                }
+            }
         }
     }
 }
