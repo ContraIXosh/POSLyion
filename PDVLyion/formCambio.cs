1b053f5044c51d8011c6ea463cbf5ff472288559
Uks <lluukks17@gmail.com>
2024-10-26 17:16:13 -0300

    Añadido basico

--- PDVLyion/formCambio.cs
+++ PDVLyion/formCambio.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static PDCLyion.formCambio;
 
 namespace PDCLyion
 {
@@ -18,16 +19,14 @@ namespace PDCLyion
         decimal montoTarjeta = 0m;
         decimal montoMP = 0m;
         decimal dinero = 0;
+ 
         public formCambio(decimal p_dinero)
         {
             InitializeComponent();
             dinero = p_dinero;
         }
 
-        private void txt_modificarprecio__TextChanged(object sender, EventArgs e)
-        {
 
-        }
 
 
         private void formCambio_Load(object sender, EventArgs e)
@@ -40,15 +39,19 @@ namespace PDCLyion
             lbl_total.Text = Convert.ToString(dinero);
         }
 
-        private void btn_cancelar_Click(object sender, EventArgs e)
-        {
-            this.Close();
-        }
+
 
         // Variables para almacenar los importes parciales
         private decimal importeEfectivo = 0;
         private decimal importeTarjeta = 0;
         private decimal importeMP = 0;
+
+ 
+
+        private void btn_cancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
 
         private void btn_cobrar_Click(object sender, EventArgs e)
         {
@@ -58,10 +61,6 @@ namespace PDCLyion
             if (!decimal.TryParse(nbud_precio.Text.Replace("$", ""), out importeParcial))
             {
             }
-
-            //if (!decimal.TryParse(lbl_cambio.Text.Replace("Falta: $", "").Replace("Total pagado.", "").Trim(), out totalAPagar))
-            //{
-            //}
 
             switch (metodoPago)
             {
@@ -75,24 +74,34 @@ namespace PDCLyion
                     break;
                 case "Mercado Pago":
                     importeMP += importeParcial;
-                    lbl_mp.Text = $"Mercado Pago: {importeMP:C}";
+                    lbl_mp.Text = $"QR: {importeMP:C}";
                     break;
             }
 
             // Calcular el total pagado
             decimal totalPagado = importeEfectivo + importeTarjeta + importeMP;
             decimal diferencia = dinero - totalPagado;
-
             if (diferencia > 0)
             {
                 lbl_total.Text = $"Falta: {diferencia:C}";
-                lbl_vuelto.Text = "Vuelto: $0"; 
+                lbl_vuelto.Text = "Vuelto: $0";
             }
             else
             {
                 lbl_total.Text = "Total pagado.";
                 lbl_vuelto.Text = $"Vuelto: {-diferencia:C}";
             }
+        }
+
+        private void btn_elimef_Click(object sender, EventArgs e)
+        {
+            lbl_efectivo.Text = string.Empty ;
+            lbl_efectivo.Text = "Efectivo: $0.00";
+        }
+
+        private void btn_elimtj_Click(object sender, EventArgs e)
+        {
+            lbl_tarjeta.Text = "Tarjeta: $0.00";
         }
     }
 }
